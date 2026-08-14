1a14648b6ae688a78483a1691b3b335d6a450959
Gabriel Gutierrez <234506380+gabrielyoshi@users.noreply.github.com>
2026-08-14 13:20:16 -0700

    chore: initialize Spring Boot backend

---
+++ backend/src/main/java/com/gabrielyoshi/randomcontextmemories/RandomcontextmemoriesApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielyoshi.randomcontextmemories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomcontextmemoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomcontextmemoriesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gabrielyoshi/randomcontextmemories/RandomcontextmemoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielyoshi.randomcontextmemories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomcontextmemoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
